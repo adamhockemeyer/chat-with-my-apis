e4fde825bf65dba938c58ebb7021e1589d24975d
Adam Hockemeyer <hock2k5@gmail.com>
2025-02-03 20:30:44 -0500

    Testing out otel logging for next js

--- src/api/app/otel_app_insights_logging.py
+++ src/api/app/otel_app_insights_logging.py
@@ -1,5 +1,7 @@
 import logging
 
+from azure.monitor.opentelemetry import configure_azure_monitor
+
 from azure.monitor.opentelemetry.exporter import (
     AzureMonitorLogExporter,
     AzureMonitorMetricExporter,
@@ -21,6 +23,8 @@ from opentelemetry.trace import set_tracer_provider
 
 # Replace the connection string with your Application Insights connection string
 #connection_string = "your-application-insights-connection-string"
+
+configure_azure_monitor()
 
 # Create a resource to represent the service/sample
 resource = Resource.create({ResourceAttributes.SERVICE_NAME: "apichat-api"})
